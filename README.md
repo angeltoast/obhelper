120c9c9ceb443cf2ab23df0bc5384b0552f3c68a
angeltoast <elizabeth@itsliz.net>
2021-10-05 18:05:44 +0100

    modified:   README.md 	renamed:    obm.sh -> obh.sh

--- README.md
+++ README.md
@@ -3,10 +3,12 @@ A Bash shell script that tries to do what obmenu used to do
 
 With obmenu no longer in the Debian repos, this bash script will try to
 include the functionality of obmenu, though it is not intended to be an
-exact copy. obhelper uses the Yad suite of GTK dialogs for display, so
+exact copy.
+
+obhelper uses the Yad suite of GTK dialogs for display, so
 make sure that you have Yad installed.
 
-Please note that obhelper is unfinished:
+Please note that obhelper is still unfinished:
    *  Some functions have not yet been coded;
    *  The application has not been widely tested, and may be incompatible
       with your system;
@@ -16,5 +18,5 @@ Please note that obhelper is unfinished:
       Any changes to data during testing are written to a temp.file for
       checking. If you wish to use a copy or dummy .xml file, pass the
       full address and name as an argument when starting obhelper, eg:
-         ./obm.sh "/absolute/path/to/your/menu.xml"
-      (the script file is called 'obm.sh' for speed of typing)
+         ./obh.sh "/absolute/path/to/your/menu.xml"
+      (the script file is called 'obh.sh' for speed of typing)
--- obh.sh
+++ obh.sh
@@ -19,7 +19,9 @@
 # Global variables
 Gnumber=0            # For returning integers from functions
 Gstring="y"          # For returning strings from functions
-declare -a OBrecord  # Array to hold the entire menu.xml
+declare -a OBfile    # Array to hold the entire menu.xml file
+declare -a OBrecord  # New array to hold a single record
+declare -a OBtemp    # New array to temporarily hold working data
 
 if [ !$1 ]; then
    XmlPath="/home/$USER/.config/openbox/menu.xml"
@@ -27,21 +29,21 @@ else
    XmlPath=$1
 fi
 
-LoadHeaders() {      # Collect contents of current menu.xml
-   readarray -t OBrecord < $XmlPath  # Copy file into array
-   MakeFile                            # Use array to prepare
-   return 0                            #  the file for display
+LoadHeaders() { # Collect contents of current menu.xml
+   readarray -t OBfile < $XmlPath    # Copy source menu.xml into array
+   MakeFile                          # Use array to prepare for display
+   return 0
 } # End LoadHeaders
 
-MakeFile() {
-   items=${#OBrecord[@]}               # Count records in the array
-   menuLevel=0                         # To manage indenting
+MakeFile() { # Prepare the display file
+   items=${#OBfile[@]}               # Count records in the array
+   menuLevel=0                       # To manage indenting
    spaces=""
    for (( i=0; i < $items; ++i ))
    do
-     # if [[ $(echo ${OBrecord[${i}]}) == "<?"* ]] || [[ $(echo ${OBrecord[${i}]}) == "<o"* ]]; then continue; fi
+     # if [[ $(echo ${OBfile[${i}]}) == "<?"* ]] || [[ $(echo ${OBfile[${i}]}) == "<o"* ]]; then continue; fi
       # Read an element from the array (remove all '>')
-      item=$(echo ${OBrecord[${i}]} | sed -e 's/>//g')
+      item=$(echo ${OBfile[${i}]} | sed -e 's/>//g')
       # Extract content after the first '<' and remove quotes
       item1=$(echo $item | cut -d'<' -f2 | sed -e 's/"//g')
       header=${item1:0:3}
@@ -59,14 +61,14 @@ MakeFile() {
       "ite") body=$(echo $item1 | cut -d'=' -f2 | sed -e 's/>//g')
             # For 'item' get the rest of the record
             j=$((i+1))   # The next line is the action   (remove all '>')
-            action=$(echo ${OBrecord[${j}]} | sed -e 's/>//g')
+            action=$(echo ${OBfile[${j}]} | sed -e 's/>//g')
             # Extract content after first '<'     and remove all quotes
             action=$(echo $action | cut -d'=' -f2 | sed -e 's/"//g')
             # Isolate the first word before the first space
             action=$(echo $action | cut -d' ' -f1)
             # The next line is the 'execute' command
             j=$((j+1))
-            execute=$(echo ${OBrecord[${j}]})
+            execute=$(echo ${OBfile[${j}]})
             # After '<' (remove all '>')
             execute=$(echo $execute | cut -d'<' -f2 | cut -d'>' -f2)
             echo "$i" >> display.list
@@ -105,17 +107,17 @@ Inset() {   # Indentation according to menu level
    return 0
 } # End Inset
 
-ShowList() {
+ShowList() { # Use Yad to display the file contents in a listbox
    while true
    do
-      selected=$(cat display.list | yad --list           \
-         --center --width=150 --height=600               \
-         --text="Select an item, then click a button"   \
+      selected=$(cat display.list | yad --list          \
+         --center --width=150 --height=600              \
+         --text="Select an object from this list, then click a button" \
          --text-align=center     \
          --title="OBhelper"      \
          --search-column=0       \
          --column="":HD          \
-         --column="Name"         \
+         --column="Label"        \
          --column="Type"         \
          --column="Action"       \
          --column="Execute"      \
@@ -125,16 +127,38 @@ ShowList() {
          --button=gtk-edit:4     \
          --button=gtk-go-up:6    \
          --button=gtk-go-down:8  \
+         --button=gtk-save:10    \
          --separator=":")
       buttonPressed=$?                       # Save button number
       i=$(echo $selected | cut -d':' -f1)    # Record number
       case $buttonPressed in
-      0) return 0 ;;
-      3) EntryForm;;
-      2) DeleteObject $i ;;
-      4) EditObject $i ;;
-      6) MoveUp $i ;;
-      8) MoveDown $i ;;
+      1) return 0 ;;
+      3) AddItem;;
+      2) if [[ ! $selected ]]; then
+            ShowMessage "Please select an object"
+            continue
+         else
+            DeleteObject $i
+         fi ;;
+      4) if [[ ! $selected ]]; then
+            ShowMessage "Please select an object"
+            continue
+         else
+            EditObject $i
+         fi ;;
+      6) if [[ ! $selected ]]; then
+            ShowMessage "Please select an object"
+            continue
+         else
+            MoveUp $i
+         fi ;;
+      8) if [[ ! $selected ]]; then
+            ShowMessage "Please select an object"
+            continue
+         else
+            MoveDown $i
+         fi ;;
+      10) SaveToFile ;;
       *) return 1
       esac
       Tidy
@@ -143,7 +167,12 @@ ShowList() {
    return 0
 } # End ShowList
 
-DeleteObject() { # $1 is selected object line number
+SaveToFile() { # TODO
+   ShowMessage "Save function not written yet"
+}
+
+DeleteObject() { # $1 is selected object line number **  TODO  **
+   ShowMessage "Delete function not written yet"
   # item="$(head -n $1 menu.xml | tail -n 1)"
   # Find in array
   # Identify enclosed objects and closing tag
@@ -153,16 +182,16 @@ DeleteObject() { # $1 is selected object line number
   return 0
 } # End DeleteObject
 
-EditObject() { # Choose action based on type of object
+EditObject() { # Choose action based on type of object   **  WIP  **
                # $1 is selected object ID number
    OBID=$1
-   item="$(echo ${OBrecord[$OBID]} | cut -d'<' -f2 | cut -c1-4)"
+   item="$(echo ${OBfile[$OBID]} | cut -d'<' -f2 | cut -c1-4)"
    case $item in
    "menu") FormatMenu $OBID  #<menu id="root-menu-885940" label="Stumped Game">
    ;;
    "item") FormatItem $OBID  # Format and save
    ;;
-   "sepa") FormatSep $OBID   # Prepare data for EntryForm
+   "sepa") FormatSep $OBID   # Prepare data for separator
    ;;
    *) return 1  # Data error
    esac
@@ -172,11 +201,11 @@ EditObject() { # Choose action based on type of object
 FormatItem() { # Load, edit and save the record
    OBID=$1
    # Extract Label (removing quotes and '>')
-   Label=$(echo ${OBrecord[$OBID]} | cut -d'=' -f2 | sed -e 's/[">]//g')
+   Label=$(echo ${OBfile[$OBID]} | cut -d'=' -f2 | sed -e 's/[">]//g')
    element=$((OBID+1)) # Read next line (removing quotes and '>'
-   Action=$(echo ${OBrecord[$element]} | cut -d'=' -f2 | sed -e 's/[">]//g')
+   Action=$(echo ${OBfile[$element]} | cut -d'=' -f2 | sed -e 's/[">]//g')
    element=$((element+1))  # Read next line to get the execute command
-   Execute=$(echo ${OBrecord[$element]} | cut -d'>' -f2 | cut -d'<' -f1)
+   Execute=$(echo ${OBfile[$element]} | cut -d'>' -f2 | cut -d'<' -f1)
    Gstring=$(yad --title='OBhelper'    \
       --form --center --on-top         \
       --width=500 --height=200         \
@@ -195,11 +224,11 @@ FormatItem() { # Load, edit and save the record
    Execute=$(printf "<execute>%s</execute>" "$item")
    FormatOutput $OBID
    pip=$OBID         # Pip related elements
-   OBrecord[$pip]="$Gstring$Label"
+   OBfile[$pip]="$Gstring$Label"
    pip=$((pip+1))    # Next element
-   OBrecord[$pip]="    $Gstring$Action"
+   OBfile[$pip]="    $Gstring$Action"
    pip=$((pip+1))    # Next elephant
-   OBrecord[$pip]="        $Gstring$Execute"
+   OBfile[$pip]="        $Gstring$Execute"
    return 0
 } # End FormatItem
 
@@ -207,9 +236,9 @@ FormatMenu() { # Load, edit and save the record
                # $1 is array record number
    OBID=$1
    # Extract ID (removing quotes)
-   MenuID=$(echo ${OBrecord[$OBID]} | sed -e 's/"//g' | cut -d'=' -f2 | cut -d'-' -f3 | cut -d' ' -f1)
+   MenuID=$(echo ${OBfile[$OBID]} | sed -e 's/"//g' | cut -d'=' -f2 | cut -d'-' -f3 | cut -d' ' -f1)
    # Extract Label (removing quotes and '>')
-   Label=$(echo ${OBrecord[$OBID]} | cut -d'=' -f3 | cut -d'-' -f3 | sed -e 's/[">]//g')
+   Label=$(echo ${OBfile[$OBID]} | cut -d'=' -f3 | cut -d'-' -f3 | sed -e 's/[">]//g')
    Gstring=$(yad --title='OBhelper'    \
       --form --center --on-top         \
       --width=500 --height=200         \
@@ -221,11 +250,11 @@ FormatMenu() { # Load, edit and save the record
    if [ $? -eq 1 ]; then return; fi    # 'Cancel' was pressed
    # Check the array for any other menus with this ID
    MenuID=$(echo $Gstring | cut -d'|' -f2)
-   items=${#OBrecord[@]}                     # Count records
+   items=${#OBfile[@]}                     # Count records
    for (( i=0; i < $items; ++i ))
    do
       if [ $i -eq $OBID ]; then continue; fi # Ignore this record
-      if [[ ${OBrecord[$i]} == *"$MenuID"* ]]; then # If substring matches
+      if [[ ${OBfile[$i]} == *"$MenuID"* ]]; then # If substring matches
          ShowMessage "Menu ID is already in use;" "Please use a different ID"
          FormatMenu $OBID                          # Restart menu edit
       fi
@@ -234,7 +263,7 @@ FormatMenu() { # Load, edit and save the record
    Label=$(echo $Gstring | cut -d'|' -f1)
    item=$(printf "<menu id=\"root-menu-%s\" label=\"%s\">" "$MenuID" "$Label")
    FormatOutput $OBID
-   OBrecord[$OBID]="$Gstring$item"
+   OBfile[$OBID]="$Gstring$item"
    return 0
 } #  FormatMenu
 
@@ -242,7 +271,7 @@ FormatSep() {  # Load, edit and save the separator
                # $1 is array record number
    OBID=$1
    # Extract Label (removing quotes and '>')
-   Label=$(echo ${OBrecord[$OBID]} | cut -d'=' -f2 | sed -e 's/["/>]//g')
+   Label=$(echo ${OBfile[$OBID]} | cut -d'=' -f2 | sed -e 's/["/>]//g')
    Gstring=$(yad --title='OBhelper'    \
       --form --center --on-top         \
       --width=500 --height=200         \
@@ -254,13 +283,13 @@ FormatSep() {  # Load, edit and save the separator
    Label=$(echo $Gstring | cut -d'|' -f1)
    item=$(printf "<menu id=\"root-menu-%s\" label=\"%s\">" "$MenuID" "$Label")
    FormatOutput $OBID
-   OBrecord[$OBID]="$Gstring$item"
+   OBfile[$OBID]="$Gstring$item"
    return 0
 } # End FormatSep
 
 FormatOutput() {  # Try to indent edited records appropriately
                   # $1 is the selected record number
-   items=${#OBrecord[@]}           # Count records in the array
+   items=${#OBfile[@]}           # Count records in the array
    menuLevel=0                     # Use menuLevel to manage indenting
    spaces=""
    for (( i=0; i < $items; ++i ))
@@ -269,7 +298,7 @@ FormatOutput() {  # Try to indent edited records appropriately
          Gstring="$spaces"
          break
       fi
-      item="${OBrecord[$i]}"        # Read an element from the array
+      item="${OBfile[$i]}"        # Read an element from the array
       item1=$(echo $item | cut -d'<' -f2 | sed -e 's/"//g') # Extract type
       header=${item1:0:3}
       case $header in
@@ -290,29 +319,37 @@ FormatOutput() {  # Try to indent edited records appropriately
    done
 }
 
-MoveUp() { # $1 is selected object line number
-  # item="$(head -n $1 menu.xml | tail -n 1)"
-  # Take care to place outside other objects.
+MoveUp() { # $1 is selected object line number ** TODO **
+   ShowMessage "Move function not written yet"
+  # OBID=$1
+  # Extract ID (see other functions)
+   # Take care with menus to move all contents
+   # Take care with items within menus to not move beyond menu bounds
+   # All types - take care to place outside other objects.
   # Copy the whole selected record into an array, then read each previous
   # line until you reach the the line prior to the top of the previous
   # record; save the line number, then start from the top of the array,
-  # copying each record into a temp file until the saved line is reached.
-  # Add the record from the temp array into the temp file, then resume
-  # copying the remaining records into the temp file.
-  # Finally rename the temp file as display.list
+  # copying each record into another array until the saved line is reached.
+  # Add the record from the temp array into the array, then resume
+  # copying the remaining records into the temporary array.
+  # Finally empty the main array and copy the temporary array into it.
   return 0
 } # End MoveUp
 
-MoveDown() { # $1 is selected object line number
-  # item="$(head -n $1 menu.xml | tail -n 1)"
-  # Take care to place outside other objects.
-  # Copy the whole selected record into an array, then read each next
-  # line until you reach the the line after the bottom of the next
-  # record; save the line number, then start from the top of the array,
-  # copying each record into a temp file until the saved line is reached.
-  # Add the record from the temp array into the temp file, then resume
-  # copying the remaining records into the temp file.
-  # Finally rename the temp file as display.list
+MoveDown() { # $1 is selected object line number   ** TODO  **
+   ShowMessage "Move function not written yet"
+  # OBID=$1
+  # Extract ID (see other functions)
+   # Take care with menus to move all contents
+   # Take care with items within menus to not move beyond menu bounds
+   # All types - take care to place outside other objects.
+  # Copy the whole selected record into an array, then read each following
+  # line until you reach the the line after the top of the next
+  # record; save the line number, then start from the end of the array,
+  # copying each record into another array until the saved line is reached.
+  # Add the record from the temp array into the array, then resume
+  # copying the remaining records into the temporary array.
+  # Finally empty the main array and copy the temporary array into it.
   return 0
 } # End MoveDown
 
@@ -330,7 +367,7 @@ Tidy() {    # Clear temporary files
 ShowMessage() {   # ShowList a message in a pop-up terminal window
                   # $1 and $2 optional lines of message text
     xterm -T " Error" -geometry 90x10+300+250 -fa monospace -fs 10 -e "echo '$1' && echo '$2' && read -p 'Please press [Enter] ...'"
-} # End DoMessage
+} # End ShowMessage
 
 Debug() {   # Insert at any point ...
             # echo " any variables "
@@ -349,10 +386,10 @@ Main() {
    done
    # Before exit, rewrite menu.xml from array and reconfigure openbox
    # First experiment with a temporary file
-   items=${#OBrecord[@]}                              # Count records
+   items=${#OBfile[@]}                              # Count records
    for (( i=0; i < $items; ++i ))
    do
-      echo "${OBrecord[$i]}" >> temp.file
+      echo "${OBfile[$i]}" >> temp.file
    done
    exit 0
 } # End Main
